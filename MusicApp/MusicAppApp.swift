90fb5c1953b2adc8b30f290d8918c048e672f080
Hidayat Abisena <plegmasena@gmail.com>
2023-07-16 14:39:01 +0700

    Initial Commit

---
+++ MusicApp/MusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAppApp.swift
+//  MusicApp
+//
+//  Created by Hidayat Abisena on 16/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
